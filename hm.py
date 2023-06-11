6884b5a132ac846d3c3c9ce1ee20776220ebc22b
AleeMoreira <alemorei12345678910@gmail.com>
2023-06-11 18:07:59 -0300

    Agregado del hola mundo y el test

---
+++ hm.py
@@ -0,0 +1,16 @@
+def hola_mundo():
+    print("Hola Mundo!")
+
+hola_mundo()
+
+""" from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hola_mundo():
+    return 'Hola Mundo!'
+
+if __name__ == '__main__':
+    app.run() """
---
+++ test_hola_mundo.py
@@ -0,0 +1,13 @@
+from hm import hola_mundo
+import pytest
+
+def test_hm():
+    assert hola_mundo=="Hola Mundo!"
+
+test_hm()
+
+""" from hm import hola_mundo
+
+def test_hm():
+    
+    assert hola_mundo()=="<h1>Hola Mundo!</h1>" """
